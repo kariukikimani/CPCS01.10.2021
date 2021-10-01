ccd40fac999448a6b4b235e757548bcf999c743a
Distinctkim <78212409+kariukikimani@users.noreply.github.com>
2021-10-01 10:54:00 +0300

    Add files via upload

---
+++ CPCSDASH/app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:09:17 2020
+
+@author: Syamanthaka B
+"""
+
+import dash
+import dash_bootstrap_components as dbc
+
+# meta_tags are required for the app layout to be mobile responsive
+"""
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}],
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+"""
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}])
+server = app.server
+app.config.suppress_callback_exceptions = True
